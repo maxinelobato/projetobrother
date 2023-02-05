f68c647ec412fbf6c93adfa04e8e297f5f440c62
maxinelobato <maxinelobato89@hotmail.com>
2023-02-05 19:52:52 -0300

    change

--- src/components/Noticias/RecadastramentoDigital.tsx
+++ src/components/Noticias/RecadastramentoDigital.tsx
@@ -1,68 +1,81 @@
 import {
   Box,
   Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Container,
   Flex,
   Heading,
   Image,
   Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 
 export function RecadastramentoDigital() {
   return (
     <>
-      <Box bgColor={'#002959'} minH={'100vh'}>
-        <Container maxW={'7xl'}>
-          <Stack spacing={12} p={4}>
-            <Heading textAlign={'center'}>Notícias SEGEF</Heading>
-            <Flex gap={4} direction={{ base: 'column', md: 'row' }}>
-              <Box justifyContent={'center'} mt={12} mb={10}>
-                <Heading
-                  as={'h1'}
-                  size={'3xl'}
-                  fontWeight={600}
-                  textAlign={{ base: 'center', md: 'left' }}
+      <Box bgColor={'#002959'}>
+        <VStack p={4}>
+          <Heading
+            as={'h1'}
+            size={['2xl', '3xl']}
+            fontWeight={600}
+            textAlign={{ base: 'center', md: 'left' }}
+          >
+            Notícias SEGEF
+          </Heading>
+        </VStack>
+        <Stack minH={'90vh'} direction={{ base: 'column', md: 'row' }}>
+          <Flex p={4} flex={1} align={'center'} justify={'center'}>
+            <Stack spacing={6} w={'full'} maxW={'lg'}>
+              <Heading
+                as={'h1'}
+                size={['2xl', '3xl']}
+                fontWeight={600}
+                textAlign={{ base: 'center', md: 'left' }}
+              >
+                Credenciamento e recadastramento digital, {''}
+                <Text as={'span'} color={'#FC8181'}>
+                  plataforma digital de gestão tributária de Ananindeua-PA
+                </Text>
+              </Heading>
+              <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  color={'blackAlpha.800'}
+                  rounded={'full'}
+                  size={'lg'}
+                  variant="solid"
+                  bgColor={'#FC8181'}
+                  _hover={{ bgColor: 'whiteAlpha.800', transition: '0.3s' }}
                 >
-                  Credenciamento e recadastramento digital, {''}
-                  <Text as={'span'} color={'#FC8181'}>
-                    plataforma digital de gestão tributária de Ananindeua-PA
-                  </Text>
-                </Heading>
-                <Stack mt={6} direction={{ base: 'column', md: 'row' }}>
-                  <Button
-                    color={'blackAlpha.800'}
-                    rounded={'full'}
-                    size={'lg'}
-                    variant="solid"
-                    bgColor={'#FC8181'}
-                    _hover={{ bgColor: 'whiteAlpha.800', transition: '0.3s' }}
-                  >
-                    Acesse Aqui
-                  </Button>
-                </Stack>
-              </Box>
-              <Box>
-                <Card
-                  overflow="hidden"
-                  variant="elevated"
-                  bgColor={'#0185C6'}
-                  borderRadius="2xl"
-                >
-                  <Image
-                    objectFit="cover"
-                    maxW={{ base: '100%', sm: '600px' }}
-                    src="/img/qrcode.png"
-                    alt="SEGEF"
-                  />
-                </Card>
-              </Box>
-            </Flex>
-          </Stack>
-        </Container>
+                  Acesse Aqui
+                </Button>
+              </Stack>
+            </Stack>
+          </Flex>
+          <Flex
+            flex={1}
+            justify={'center'}
+            align={'center'}
+            position={'relative'}
+            w={'full'}
+            p={4}
+          >
+            <Box
+              position={'relative'}
+              rounded={'2xl'}
+              boxShadow={'2xl'}
+              overflow={'hidden'}
+              bgColor={'#0185C6'}
+              borderRadius="2xl"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: '100%', md: '600px' }}
+                src="/img/qrcode.png"
+                alt="SEGEF"
+              />
+            </Box>
+          </Flex>
+        </Stack>
       </Box>
     </>
   );
